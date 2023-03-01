5621e7e827fccb4e0f1fb7d335b3b97e2b9c64fc
Max Bashkirov <122776934+QAtactics@users.noreply.github.com>
2023-03-01 07:08:50 +0300

    Update qa studio.cy.js

--- new_cypress-main/cypress/e2e/qa studio.cy.js
+++ new_cypress-main/cypress/e2e/qa studio.cy.js
@@ -1,5 +1,5 @@
 
-describe('Тестирование покупки QA Studio (кол-во с кликом)', function () {
+describe('Тестирование покупки QA Studio', function () {
     
     it('Позитивная Авторизация', function () {
         cy.visit(' https://testqastudio.me/');
@@ -12,7 +12,6 @@ describe('Тестирование покупки QA Studio (кол-во с кл
         cy.get('.home > span').click()
         cy.get('.post-11337 > .product-inner > .product-thumbnail > .woocommerce-LoopProduct-link > .attachment-woocommerce_thumbnail').click()
         cy.get('.summary > .cart > .product-button-wrapper > .single_add_to_cart_button').click()
-        cy.wait(2000)
         cy.get('.checkout').click();
         cy.get('#billing_first_name').type('Максим');
         cy.get('#billing_last_name').type('Башкиров');
@@ -21,10 +20,9 @@ describe('Тестирование покупки QA Studio (кол-во с кл
         cy.get('#billing_state').type('Манхэттенская область');
         cy.get('#billing_postcode').type('111111');
         cy.get('#billing_phone').type('11111111');
-        cy.get('#billing_email').type('qastudio@mail.ru');
-        cy.get('#order_comments').type('Арина привет)');
+        cy.get('#billing_email').type('11111@mail.ru');
+        cy.get('#order_comments').type('111111111)');
         cy.get('#place_order').click();
-        cy.wait(2000)
         cy.contains('Ваш заказ принят.').should('be.visible')
 
     })
